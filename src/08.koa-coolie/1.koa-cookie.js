a6b3831513d0ceff09dafd25e93482f8576f2f25
lfp1024 <lfp1024@126.com>
2020-07-27 00:45:22 +0800

    add cookie option

--- src/08.koa-coolie/1.koa-cookie.js
+++ src/08.koa-coolie/1.koa-cookie.js
@@ -3,6 +3,9 @@ const Router = require('koa-router')
 const render = require('koa-art-template')
 const path = require('path')
 
+const CookieDateEpoch = 'Thu, 01 Jan 1970 00:00:00 GMT'
+const COOKIE_EXP_DATE = new Date(CookieDateEpoch)
+
 const app = new Koa()
 const router = new Router({ prefix: '/cookie' })
 
@@ -16,12 +19,26 @@ router.get('/', async (ctx) => {
   // 在响应报文中通过 Set-Cookie 字段传递给客户端
   ctx.cookies.set('username', 'lfp', {
     maxAge: 60 * 1000 * 60 * 24,
+    // maxAge: -1,
+    // expires: new Date(2020, 6, 2), // 同时存在，则maxAge优先级高，里面会重新赋值
+
+    // 最终没有 maxAge 字段，只有expires字段
+    // 将值转换为数字计算
+    // if (this.maxAge) this.expires = new Date(Date.now() + this.maxAge);
+
+    // true == 1, 条件成立，expires = Date.now() + 1，立即过期
+    // 负数        条件成立，expires = Date.now() + 负数，立即过期
+    // 正数（毫秒）  条件成立，expires = Date.now() + 正数，指定时间后过期（1000ms闪一下便过期消失）
+    // false 0 '' null NaN undefined, 条件不成立，expires = undefined => session cookie
+    // 非空字符串，  条件成立，但是 new Date() 返回 Invalid Date，expires = Invalid Date => session cookie
+
     // path: '/cookie/news', // 只有路径匹配才能
     // domain:'.baidu.com' // 此时 只有访问百度相关的网站，baike.baidu.com tieba.baidu.com 才可以携带Cookie
     httpOnly: false // 在客户端可以通过 JS代码访问
 
   })
   await ctx.render('index')
+  console.log('11')
 
   ctx.body = '设置cookie'
 })
